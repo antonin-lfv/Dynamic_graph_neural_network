175d82f05365d29e365de9f47951a2c5dc52cd05
antonin-lfv <antoninlefevre45gicloud.com>
2022-05-07 22:03:08 +0200

    fix plotGraph issue

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ G.addNeuron(Neuron(vecteur=type_1[3]))
 G.addNeuron(Neuron(vecteur=type_1[5]))
 G.addNeuron(Neuron(vecteur=type_2[1]))
 G.addNeuron(Neuron(vecteur=type_2[4]))
-G.plotGraph()
+# G.plotGraph()
 
+print(G.neurons)
 
 # G1 : 0, 3, 4, 6, 7
 # G2 : 1, 2, 5, 8, 9
