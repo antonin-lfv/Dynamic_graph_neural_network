d18166587f947d24e5aa5e1cfcee19cc311b2846
Antonin <antoninlefevre45@icloud.com>
2022-05-09 14:26:31 +0200

    config1

--- config2_article.py
+++ config2_article.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 from utils.classes import *
 
 """ Version article """
@@ -10,14 +8,19 @@ from utils.classes import *
 s = np.sin
 c = np.cos
 
-
-def random_signal():
-    signal = random.uniform(0, 1)*s(np.linspace(0, 10, ConstGraph_article.INPUT_SIZE_CONFIG_2))
-    for i in range(5):
-        signal += (-1)**random.randint(1, 2)*random.uniform(0, 1)*s(np.linspace(0, 10, ConstGraph_article.INPUT_SIZE_CONFIG_2))
-    return signal
+pi = np.pi
 
 
-fct = {}
-for i in range(16):
-    fct[i] = random.uniform(0,1)
+def dict_of_signal():
+    def random_signal():
+        signal = random.uniform(0, 1)*s(np.linspace(0, 10, ConstGraph_article.INPUT_SIZE_CONFIG_2))
+        for i in range(5):
+            signal += (-1)**random.randint(1, 2)*random.uniform(0, 1)*s(pi*i*np.linspace(0, 10, ConstGraph_article.INPUT_SIZE_CONFIG_2))
+        return signal
+    fct = {}
+    for i in range(16):
+        fct[i] = random_signal()
+    return fct
+
+
+signaux = dict_of_signal()
