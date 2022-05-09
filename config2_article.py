6857efd3c4594d48982c4c1c00831f94d43bc879
Antonin <antoninlefevre45@icloud.com>
2022-05-09 14:51:44 +0200

    config1

--- config2_article.py
+++ config2_article.py
@@ -8,19 +8,31 @@ from utils.classes import *
 s = np.sin
 c = np.cos
 
+# constantes
 pi = np.pi
+np.random.seed(1)
+
+# intervalle signal
+inter = np.linspace(0, 10, ConstGraph_article.INPUT_SIZE_CONFIG_2)
 
 
 def dict_of_signal():
     def random_signal():
-        signal = random.uniform(0, 1)*s(np.linspace(0, 10, ConstGraph_article.INPUT_SIZE_CONFIG_2))
+        signal = (-1)**random.randint(1, 2)*random.uniform(0, 1)*s(inter)
         for i in range(5):
-            signal += (-1)**random.randint(1, 2)*random.uniform(0, 1)*s(pi*i*np.linspace(0, 10, ConstGraph_article.INPUT_SIZE_CONFIG_2))
+            signal += (-1)**random.randint(1, 2)*random.uniform(0, 1)*s(pi*i*inter)
         return signal
     fct = {}
-    for i in range(16):
+    for i in range(9):
         fct[i] = random_signal()
     return fct
 
 
 signaux = dict_of_signal()
+
+
+def affichage_signaux():
+    fig = go.Figure()
+    for sign in signaux.values():
+        fig.add_scatter(y=sign, x=inter)
+    plot(fig)
